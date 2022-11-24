dd190f0ae937b4314e71614ca0b5630d21d0039b
DavidGraber <david.graber@bluewin.ch>
2022-11-24 09:00:49 +0100

    adapted to diffpool model

--- c_GraphPatch.py
+++ c_GraphPatch.py
@@ -2,38 +2,32 @@ import numpy as np
 
 class GraphPatch:
 
-    '''Class storing the data of an extracted surface patch graph, including coordinates of the graph nodes, 
-    the features, the adjacency information, the edge_attributes and the fitness and the name of the mutant'''
+    '''Class storing the data of an extracted surface patch graph, including coordinates of the graph nodes (pos), 
+    the features (x), the adjacency information (adj), the edge weights (w) and the fitness (y) and the name of the mutant'''
 
-    def __init__(self, feature_matrix, A, edge_index, edge_weight, edge_data, fitness_value, coords, mutant_name):
-        self.coords = coords
-        self.edge_data = edge_data
-        self.A = A
-        self.edge_index = edge_index
-        self.edge_weight = np.reshape(edge_weight, (edge_weight.shape[0], 1))
-        self.features = feature_matrix
-        self.fitness = np.asarray(fitness_value, dtype=np.float64)
+    def __init__(self, x, adj, w, y, pos, mutant_name):
+        self.pos = pos
+        self.adj = adj
+        self.w = w
+        self.x = x
+        self.y = np.asarray(y, dtype=np.float64)
         self.mutant = mutant_name
 
     def num_nodes(self):
-        return len(self.coords)
-
-    def num_edges(self):
-        return self.edge_index.shape[1]
+        return len(self.pos)
 
     def num_features(self):
-        return self.features.shape[1]
+        return self.x.shape[1]
 
     def __str__(self):
         string = '\
             Number of Nodes: {n}\n\
             Features: {f}\n\
             Adjacency Matrix: {a}\n\
-            Edge Weights (Geodesic Distances): {w}\n\
-            Edge Index: {i}\n\
-            Fitness: {fit}\n\
+            Edge Weights (Softmin Geodesic Distances): {we}\n\
+            Label: {fit}\n\
             Coordinates of Points: {c}\n\
             Mutant Name: {name}'\
-            .format(n = self.coords.shape[0], f = self.features.shape, a = self.A.shape, w = self.edge_weight.shape, \
-            i = self.edge_index.shape, fit=self.fitness, c = self.coords.shape, name = self.mutant)
+            .format(n = self.pos.shape[0], f = self.x.shape, a = self.adj.shape, we = self.w.shape, \
+            i = self.edge_index.shape, fit=self.y, c = self.pos.shape, name = self.mutant)
         return string
